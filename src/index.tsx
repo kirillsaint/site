54b4cb1e952f246c3b34703b2fca970d9906adf5
kirillsaint <kirillsaint@wtfuck.tk>
2022-10-08 20:25:24 +0600

    Dark appearance in ConfigProvider

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<ConfigProvider>
+		<ConfigProvider appearance="dark">
 			<AdaptivityProvider>
 				<App />
 			</AdaptivityProvider>
